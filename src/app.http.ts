dc2cf1c1594128bef76a898ab0c8f07e89de5699
Mauricio Cantor <mauricio.cantor.p@hotmail.com>
2023-12-26 21:55:06 -0500

    First Commit

---
+++ src/app.http.ts
@@ -0,0 +1,28 @@
+
+import * as http from "http";
+import * as fs from "fs";
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write('<h1>   hola mundo </h1>');
+
+    if(req.url?.includes('/js/')){
+        const htmlFile = fs.readFileSync('./public'+req.url,'utf-8');
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
+        res.end(htmlFile);
+    }else if(req.url?.includes('/css/')){
+        const htmlFile = fs.readFileSync('./public'+req.url,'utf-8');
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(htmlFile);
+    }else if(req.url === '/'){
+        const htmlFile = fs.readFileSync('./public/index.html','utf-8');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(htmlFile);
+        res.end();
+    }
+});
+
+server.listen(3000, ()=>{
+    console.log('Server running 3000');
+});
---
+++ src/presentation/server.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import path from "node:path";
+
+interface Options {
+    port: number;
+    public_path?: string
+}
+
+export class Server{
+
+    private app = express();
+    private readonly port: number;
+    private readonly public_path: string;
+
+    constructor(options: Options) {
+        const { port, public_path = 'public'}=options;
+        this.port=port;
+        this.public_path = public_path;
+    }
+    async start(){
+        //middleware
+
+        //Public folder
+        this.app.use(express.static(this.public_path));
+
+        this.app.get('*',(req,res)=>{
+            const indexPath = path.join(__dirname,'../../public/index.html');
+            res.sendFile(indexPath);
+        });
+
+        this.app.listen(this.port, ()=>{
+            console.log('Server Running');
+        });
+    }
+
+
+
+
+}
